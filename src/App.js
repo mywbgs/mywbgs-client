c5186b71580cc84e8cb1b8ac0a9c35be8f6d430b
Nicholas Bush <nickdbush@hotmail.co.uk>
2017-11-19 17:34:34 +0000

    Added analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider, connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './screens/Home';
 import Login from './screens/Login';
@@ -10,22 +10,23 @@ import HomeworkView from './screens/homework/HomeworkView';
 // import Menu from './screens/Menu';
 import Calendar from './screens/Calendar';
 import Timetable from './screens/Timetable';
+import withTracker from './withTracker';
 
 const App = props => {
     const router = (
-        <Router>
+        <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/homework" component={Homework}/>
-                <Route exact path="/homework/new" component={HomeworkEdit}/>
-                <Route exact path="/homework/:id/edit" component={HomeworkEdit}/>
-                <Route exact path="/homework/:id/view" component={HomeworkView}/>
-                {/* <Route exact path="/menu" component={Menu}/> */}
-                <Route exact path="/calendar" component={Calendar}/>
-                <Route exact path="/timetable" component={Timetable}/>
+                <Route exact path="/" component={withTracker(Home)}/>
+                <Route exact path="/login" component={withTracker(Login)}/>
+                <Route exact path="/homework" component={withTracker(Homework)}/>
+                <Route exact path="/homework/new" component={withTracker(HomeworkEdit)}/>
+                <Route exact path="/homework/:id/edit" component={withTracker(HomeworkEdit)}/>
+                <Route exact path="/homework/:id/view" component={withTracker(HomeworkView)}/>
+                {/* <Route exact path="/menu" component={withTracker(Menu)}/> */}
+                <Route exact path="/calendar" component={withTracker(Calendar)}/>
+                <Route exact path="/timetable" component={withTracker(Timetable)}/>
             </Switch>
-        </Router>
+        </BrowserRouter>
     );
     return (
         <Provider store={props.store}>
