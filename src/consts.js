bfc6ce1049697001f2201577df52929622968363
Nicholas Bush <nickdbush@hotmail.co.uk>
2017-11-23 23:14:07 +0000

    Changed menu colour

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export const HOMEWORK_COLOUR = '#009688';
 export const CALENDAR_COLOUR = '#00897B';
 export const TIMETABLE_COLOUR = '#00796B';
-export const MENU_COLOUR = '#004D40';
+export const MENU_COLOUR = '#006E5B';
