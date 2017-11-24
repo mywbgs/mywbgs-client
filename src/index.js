13ddce934959352e42c597601a81c2bf3c6552f5
Nicholas Bush <nickdbush@hotmail.co.uk>
2017-11-24 12:27:45 +0000

    Added better error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Raven from 'raven';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css';
@@ -7,5 +8,15 @@ import './index.css';
 
 import store from './store';
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+try {
+    ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+} catch(err) {
+    const state = store.getState();
+    Raven.setUserContext({
+        username: state.datastore.profile.username,
+        email: state.datastore.profile.email
+    });
+    Raven.captureException(err);
+    Raven.showReportDialog();
+}
 registerServiceWorker();
