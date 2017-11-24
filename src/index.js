3a23b21b4a9761cde71ca0ce073ed7333bbb0fff
Nicholas Bush <nickdbush@hotmail.co.uk>
2017-11-24 13:29:26 +0000

    Emergency bugfix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Raven from 'raven';
+import * as Raven from 'raven-js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 
 import store from './store';
+
+if(process.env.NODE_ENV === 'production') {
+    Raven.config('https://bcdf160b5c1647d8b4bd4de442eb74c4@sentry.io/247337').install();
+}
 
 try {
     ReactDOM.render(<App store={store}/>, document.getElementById('root'));
