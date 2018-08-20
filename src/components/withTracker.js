1501905cca39c64997d9fc9e003edb78040019a4
Nicholas Bush <nickdbush@hotmail.co.uk>
2018-08-20 15:46:36 +0100

    View homework since July

--- src/components/withTracker.js
+++ src/components/withTracker.js
@@ -1,43 +1,45 @@
-import React, {
-    Component
-} from 'react';
-import ReactGA from 'react-ga';
-import ErrorBoundary from './ErrorBoundary';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import ErrorBoundary from "./ErrorBoundary";
 
 const withTracker = (WrappedComponent, options = {}) => {
-    const trackPage = page => {
-        ReactGA.set({
-            page,
-            ...options,
-        });
-        ReactGA.pageview(page);
-    };
+  const trackPage = page => {
+    ReactGA.set({
+      page,
+      ...options
+    });
+    ReactGA.pageview(page);
+  };
 
-    const HOC = class extends Component {
-        componentDidMount() {
-            let page = this.props.location.pathname;
-            const regex = /\/homework\/(\d+)(?:\/.+)?/g.exec(page);
-            if(regex && regex[1]) {
-                page = page.replace(`/homework/${regex[1]}`, '/homework');
-            }
-            trackPage(page);
-        }
+  const HOC = class extends Component {
+    componentDidMount() {
+      let page = this.props.location.pathname;
+      const regex = /\/homework\/(\d+)(?:\/.+)?/g.exec(page);
+      if (regex && regex[1]) {
+        page = page.replace(`/homework/${regex[1]}`, "/homework");
+      }
+      trackPage(page);
+    }
 
-        componentWillReceiveProps(nextProps) {
-            const currentPage = this.props.location.pathname;
-            const nextPage = nextProps.location.pathname;
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname;
+      const nextPage = nextProps.location.pathname;
 
-            if (currentPage !== nextPage) {
-                trackPage(nextPage);
-            }
-        }
+      if (currentPage !== nextPage) {
+        trackPage(nextPage);
+      }
+    }
 
-        render() {
-            return <ErrorBoundary><WrappedComponent {...this.props}/></ErrorBoundary>;
-        }
-    };
+    render() {
+      return (
+        <ErrorBoundary>
+          <WrappedComponent {...this.props} />
+        </ErrorBoundary>
+      );
+    }
+  };
 
-    return HOC;
+  return HOC;
 };
 
 export default withTracker;
